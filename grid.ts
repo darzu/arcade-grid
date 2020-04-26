3a33ccd8130defaeb75d5ea2661ebd2960000169
Daryl Zuniga <Daryl.Zuniga@gmail.com>
2020-04-26 08:47:29 -0700

    progress on grid placement and movement

--- grid.ts
+++ grid.ts
@@ -5,9 +5,14 @@ function locCol(l: tiles.Location): number {
     return (l as any)._col
 }
 
-//% color=#c96a3a icon="\uf00a"
+const DATA_ROW = "__gridRow"
+const DATA_COL = "__gridCOL"
+
+//% color=#8c6049 icon="\uf00a"
 //% groups='["Grid"]'
 namespace grid {
+    let _currentGrid: Grid;
+
     export class Grid {
         public sprites: Sprite[][];
 
@@ -24,24 +29,95 @@ namespace grid {
         public place(sprite: Sprite, loc: tiles.Location) {
             if (sprite.flags & sprites.Flag.Destroyed)
                 return
+            this.remove(sprite);
             const x = locCol(loc)
             const y = locRow(loc)
+            this.remove(this.sprites[x][y])
             this.sprites[x][y] = sprite
+            const d = sprite.data()
+            d[DATA_COL] = x
+            d[DATA_ROW] = y
             loc.place(sprite)
+        }
+
+        public remove(sprite: Sprite) {
+            if (!sprite)
+                return
+            const d = sprite.data()
+            const c: number = d[DATA_COL]
+            const r: number = d[DATA_ROW]
+            if (c === undefined || r === undefined)
+                return
+            this.sprites[c][r] = undefined
         }
     }
 
-    //% block
-    //% blockSetVariable=spriteGrid
-    export function newSpriteGrid(): Grid {
-        const tm = game.currentScene().tileMap
-        if (!tm)
-            throw "No tilemap set; cannot create a Grid";
-        return new Grid(tm)
+    function currentGrid(): Grid {
+        if (!_currentGrid) {
+            const tm = game.currentScene().tileMap
+            if (!tm)
+                throw "Cannot use sprite grid; no tilemap has been set";
+            _currentGrid = new Grid(tm)
+        }
+        return _currentGrid;
     }
 
-    //% block="in %grid=variables_get(spriteGrid) place %sprite=variables_get(mySprite) on top of $loc=mapgettile"
-    export function place(grid: Grid, sprite: Sprite, loc: tiles.Location) {
-        return grid.place(sprite, loc);
+    //% block="grid place %sprite=variables_get(mySprite) on top of $loc=mapgettile"
+    export function place(sprite: Sprite, loc: tiles.Location) {
+        currentGrid().place(sprite, loc);
+    }
+
+    //% block="grid location of $sprite=variables_get(mySprite)"
+    export function getLocation(sprite: Sprite): tiles.Location {
+        //return tiles.getTileLocation(screenCoordinateToTile(s.x), screenCoordinateToTile(s.y));
+        let r = sprite.data()[DATA_ROW]
+        let c = sprite.data()[DATA_COL]
+        if (r === undefined || c === undefined)
+            return null
+        return game.currentScene().tileMap.getTile(c, r)
+    }
+
+    //% block="add $loc=mapgettile cols $columns rows $rows"
+    export function add(loc: tiles.Location, columns: number, rows: number): tiles.Location {
+        const c = locCol(loc)
+        const r = locRow(loc)
+        return game.currentScene().tileMap.getTile(c + columns, r + rows)
+    }
+
+    //% block="move $sprite=variables_get(mySprite) by cols $columns rows $rows"
+    export function move(sprite: Sprite, columns: number, rows: number) {
+        const loc = getLocation(sprite)
+        const c = locCol(loc)
+        const r = locRow(loc)
+        const newLoc = game.currentScene().tileMap.getTile(c + columns, r + rows)
+        place(sprite, newLoc);        
+    }
+
+    //% block="swap $sprite1=variables_get(mySprite) and $sprite2=variables_get(mySprite2)"
+    export function swap(sprite1: Sprite, sprite2: Sprite) {
+        let l1 = getLocation(sprite1)
+        if (!l1)
+            return
+        let l2 = getLocation(sprite2)
+        if (!l2)
+            return
+        place(sprite1, l2)
+        place(sprite2, l1)
+    }
+
+    //% block="grid move $sprite=variables_get(mySprite) with buttons"
+    export function moveWithButtons(sprite: Sprite) {
+        controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+            move(sprite, 0, -1)
+        })
+        controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+            move(sprite, 0, 1)
+        })
+        controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+            move(sprite, -1, 0)
+        })
+        controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+            move(sprite, 1, 0)
+        })
     }
 }
